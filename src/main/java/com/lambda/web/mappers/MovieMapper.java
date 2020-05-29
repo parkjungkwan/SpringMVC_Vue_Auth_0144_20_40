b301233383acc3d43aa6f766490ec31dfacafbbf
pakjkwan <pakjkwan@gmail.com>
2020-05-29 15:20:39 +0900

    15:20

--- src/main/java/com/lambda/web/mappers/MovieMapper.java
+++ src/main/java/com/lambda/web/mappers/MovieMapper.java
@@ -1,5 +1,9 @@
 package com.lambda.web.mappers;
 
-public class MovieMapper {
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieMapper {
+    public void  insertMovie();
 
 }
